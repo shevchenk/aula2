ac47f236b6233785fce6ea7bbd132ad92ee68a01
shevchenk <jorgeshevchenk@gmail.com>
2019-11-25 20:33:57 -0500

    act

--- resources/views/proceso/master/gestor/form/aprobados.php
+++ resources/views/proceso/master/gestor/form/aprobados.php
@@ -1,7 +1,7 @@
 <div class="modal" id="ModalAprobados" tabindex="-1" role="dialog">
     <div class="modal-dialog modal-lg">
         <div class="modal-content">
-            <div class="modal-header btn-info">
+            <div class="modal-header btn-success">
                 <button type="button" class="close" data-dismiss="modal" aria-label="Close">
                     <span aria-hidden="true">&times;</span></button>
                 <h4 class="modal-title">Listado de alumno aprobados</h4>
