ad4c5a8c1193a83b24f219fa06bbae43177856c6
shevchenk <jorgeshevchenk@gmail.com>
2020-03-03 20:46:19 -0500

    act

--- app/Http/Controllers/Api/ApiCurso.php
+++ app/Http/Controllers/Api/ApiCurso.php
@@ -155,7 +155,7 @@ class ApiCurso extends Controller
               //dd($cli_links);
               $key = $this->curl($cli_links->url);
 
-              dd($key);
+              dd($r);
               
               if(isset($key->data->key) AND $key->data->key == $tab_cli->key) // Se iguala el KEY del Cliente con el Key del Servidor
               {
